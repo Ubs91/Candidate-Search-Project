4bc925ce371bbbd48f046e717cc9e4bb9c7c45b5
LAUM <luisubidia@gmail.com>
2025-04-08 19:50:50 -0600

    vite config file update to provide access to render

--- Develop/vite.config.ts
+++ Develop/vite.config.ts
@@ -5,4 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: Number(process.env.PORT) || 3000,
+    allowedHosts: [
+      'candidate-search-project.onrender.com',
+      '*.onrender.com'
+    ]
+  }
 });
